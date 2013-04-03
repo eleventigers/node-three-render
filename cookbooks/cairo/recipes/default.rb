136729659842d60c0f4b329bc8d5cf3fa222a0a1
eleventigers <phonodelik@gmail.com>
2013-04-03 10:39:13 +0100

    comment valgrind

--- cookbooks/cairo/recipes/default.rb
+++ cookbooks/cairo/recipes/default.rb
@@ -1,6 +1,8 @@
 packages = value_for_platform(
     "ubuntu" => {
         "default" => ["pkg-config", "libpixman-1-dev", "libcairo2-dev", "libpng12-dev", "libfreetype6-dev", "libfontconfig1-dev", "python-ldap", "python-cairo"]
+        # Could not resolve valgrind so I took it off - not sure if we need it.
+        # "default" => ["pkg-config", "valgrind", "libpixman-1-dev", "libcairo2-dev", "libpng12-dev", "libfreetype6-dev", "libfontconfig1-dev", "python-ldap", "python-cairo"]
     },
     "scientific" => {
         "default" => ["pkgconfig", "valgrind-devel", "libpng-devel", "freetype-devel", "fontconfig-devel", "pycairo-devel", "bitmap-fonts"]
