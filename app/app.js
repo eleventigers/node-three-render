34377c2c2b685b2ad00aed2b47667d56683136f3
eleventigers <phonodelik@gmail.com>
2013-04-03 10:35:08 +0100

    cleanup

--- app/app.js
+++ app/app.js
@@ -1,16 +1,25 @@
 var Canvas = require('canvas')
   , canvas = new Canvas(200,200)
-  , ctx = canvas.getContext('2d');
+  , ctx = canvas.getContext('2d')
+  , fs = require('fs')
+  , out = fs.createWriteStream(__dirname + '/text.png')
+  , stream = canvas.pngStream();
 
 ctx.font = '30px Impact';
 ctx.rotate(.1);
-ctx.fillText("Awesome!", 50, 100);
+ctx.fillText("Boom!", 50, 100);
 
-var te = ctx.measureText('Awesome!');
+var te = ctx.measureText('Boom!');
 ctx.strokeStyle = 'rgba(0,0,0,0.5)';
 ctx.beginPath();
 ctx.lineTo(50, 102);
 ctx.lineTo(50 + te.width, 102);
 ctx.stroke();
 
-console.log('<img src="' + canvas.toDataURL() + '" />');
+stream.on('data', function(chunk){
+  out.write(chunk);
+});
+
+stream.on('end', function(){
+  console.log('saved png');
+});
